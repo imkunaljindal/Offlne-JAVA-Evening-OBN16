8954771e64258d6404e78f7dd7c0ce41ea05494e
Kunal Jindal <kunaljindal@mnnit.ac.in>
2025-08-20 22:05:25 +0530

    Intro to programming

---
+++ Module 1/Intro To Programming/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("A");
+        System.out.print("B");
+
+        byte a = 10;
+        short b = 100;
+        int c = 4823093;
+        long d = 22332563654654L;
+        char e = 'a';
+        boolean f = true;
+        double g = 5.6;
+        float k = 3.6f;
+
+        int x;  // declaration
+        x = 10; // initialization
+
+        int y = 20; // dec + init
+        System.out.println(y);
+    }
+}
